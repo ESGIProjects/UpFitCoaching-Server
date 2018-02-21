51d86074d73d6d7e7d95b32d7cc4c513c676bf43
Kévin Le <tensa@MacBook-Pro-de-Kevin.local>
2018-02-21 18:48:20 +0100

    Fix Delete and Send with new database structure

--- messenger.go
+++ messenger.go
@@ -31,8 +31,8 @@ func GetMessages(w http.ResponseWriter, r *http.Request) {
 func SendMessage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	db := dbConn()
-	_, err := db.Query("INSERT INTO Message (idSender,idReceiver,body,timestamp) VALUES ('" + r.PostFormValue("idSender") +
-		"','" +r.PostFormValue("idReceiver") + "','" + r.PostFormValue("body") + "','" +r.PostFormValue("timestamp") + "')")
+	_, err := db.Query("INSERT INTO Message (idSender,idReceiver,body,timestamp,deletedSender,deletedReceiver) VALUES ('" + r.PostFormValue("idSender") +
+		"','" +r.PostFormValue("idReceiver") + "','" + r.PostFormValue("body") + "','" +r.PostFormValue("timestamp") + "','0','0')")
 	if err == nil{
 		w.WriteHeader(http.StatusCreated)
 	} else {
@@ -43,9 +43,16 @@ func SendMessage(w http.ResponseWriter, r *http.Request) {
 func DeleteMessages(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	db := dbConn()
-	test,_ := r.URL.Query()["id"]
-	_, err := db.Query("DELETE FROM Message WHERE id='" + test[0] + "'")
-	if err == nil{
+	id,_ := r.URL.Query()["id"]
+	idUser,_ := r.URL.Query()["idUser"]
+	var idSender, idReceiver, deletedSender, deletedReceiver string
+	db.QueryRow("SELECT idSender,idReceiver,deletedSender,deletedReceiver FROM Message WHERE id='" + id[0] + "'").Scan(&idSender,
+	&idReceiver,&deletedSender,&deletedReceiver)
+	if idUser[0] == idSender && deletedSender == "0" {
+		db.QueryRow("UPDATE Message SET deletedSender='1' WHERE id='" + id[0] + "'")
+		w.WriteHeader(http.StatusOK)
+	} else if idUser[0] == idReceiver && deletedReceiver == "0"{
+		db.QueryRow("UPDATE Message SET deletedReceiver='1' WHERE id='" + id[0] + "'")
 		w.WriteHeader(http.StatusOK)
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
