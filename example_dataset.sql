7245511a959e17d7588bd8eb5c8c68f3e56ab535
Jason Pierna <siilver777@me.com>
2018-04-06 11:18:29 +0200

    Adds database and example_dataset SQL files

---
+++ example_dataset.sql
@@ -0,0 +1,7 @@
+-- Coach
+INSERT INTO coaches (mail, password, firstName, lastName, address, city, phoneNumber) VALUES ('coach@test.fr', 'coachtest', 'Coach', 'Super', '1, Infinite Loop', 'Cupertino', '0123456789');
+
+-- Users
+INSERT INTO users (type, mail, password, firstName, lastName, birhDate, city, phoneNumber) VALUES (1, 'jasonpierna@icloud.com', 'motdepasse', 'Jason', 'Pierna', '1995-08-07', 'Garges-lès-Gonesse', '0123456789');
+INSERT INTO users (type, mail, password, firstName, lastName, birhDate, city, phoneNumber) VALUES (1, 'kevints.le@gmail.com', 'motdepasse', 'Kévin', 'Le', '1994-12-29', 'Ermont', '0123456789');
+INSERT INTO users (type, mail, password, firstName, lastName, birhDate, city, phoneNumber) VALUES (1, 'maeva.malih@gmail.com', 'motdepasse', 'Maëva', 'Malih', '1994-02-04', 'Saint-Brice', '0123456789');
